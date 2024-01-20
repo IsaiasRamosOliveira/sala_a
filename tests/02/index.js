889e387dbab7ec41e9a591f78b894f20b4827a05
Isaías Ramos <isaias.ramos.olive@gmail.com>
2024-01-20 12:14:03 -0300

    test done

---
+++ tests/02/index.js
@@ -0,0 +1,38 @@
+const users = [
+  {
+    common: [
+      {
+        name: "user 1",
+        age: 12,
+        email: "user1@gmail.com"
+      },
+      {
+        name: "user 2",
+        age: 16,
+        email: "user2@gmail.com"
+      },
+      {
+        name: "user 3",
+        age: 13,
+        email: "user3@gmail.com"
+      },
+    ],
+    premium: [
+      {
+        name: "user 4",
+        age: 12,
+        email: "user4@gmail.com"
+      },
+      {
+        name: "user 5",
+        age: 16,
+        email: "user5@gmail.com"
+      },
+      {
+        name: "user 6",
+        age: 13,
+        email: "user6@gmail.com"
+      },
+    ]
+  }
+]
